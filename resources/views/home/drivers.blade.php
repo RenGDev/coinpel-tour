3bd8ba0a7a6eb376ed7b752e72e1fc9c639d53a0
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-08-17 08:08:25 -0300

    FEAT: add home pages

---
+++ resources/views/home/drivers.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.home')
+
+@section('title', 'Clientes')
+
+@section('content')
+    <h1>Lista de Motoristas</h1>
+    <p>Aqui vai a listagem de motoristas...</p>
+@endsection
---
+++ resources/views/home/users.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.home')
+
+@section('title', 'CoinTur | Clientes')
+
+@section('content')
+    <h1>Lista de Clientes</h1>
+    <p>Aqui vai a listagem de clientes...</p>
+@endsection
