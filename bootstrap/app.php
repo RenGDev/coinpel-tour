293ab7b8fee3397c6e555ea7c6fa306f2b161bd9
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2025-08-20 18:58:23 -0300

    FEAT: add auth and authorization route funcionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\Authorization;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'is_admin' => Authorization::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
